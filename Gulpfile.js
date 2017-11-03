9ae42b682abeddf5e0b0b4f06143853ebcfe313e
nicokant <niccolocantu@gmail.com>
2017-11-03 13:00:43 +0100

    setup npm publishing

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,24 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sassLint = require('gulp-sass-lint');
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('sass', () => gulp.src('sass/**/*.s[ac]ss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./css/')));
+  .pipe(gulp.dest('./css')));
 
 gulp.task('lint', () => gulp.src('sass/**/*.s[ac]ss')
   .pipe(sassLint())
   .pipe(sassLint.failOnError()));
 
+gulp.task('prod', () => gulp.src('sass/**/*.s[ac]ss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(rename('readable.css'))
+  .pipe(gulp.dest('./css/'))
+  .pipe(cssmin())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest('./css/')));
+
 gulp.task('watch', () => gulp.watch('sass/**/*.s[ac]ss', ['lint', 'sass']));
-gulp.task('default', ['lint', 'sass']);
+gulp.task('default', ['lint', 'prod']);
